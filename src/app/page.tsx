9df40bc6749eea975cec754ec2d75f7b9eec1920
Nguyễn Á Luân <luan.na@urbox.vn>
2026-06-20 18:45:28 +0700

    ifx: add animaton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Layout from "@/components/layout/Layout";
 
 import About from "@/sections/About";
 import Contact from "@/sections/Contact";
-import HowItWork1 from "@/sections/HowItWork1";
 import Logo from "@/sections/Logo";
 import Pricing from "@/sections/Pricing";
 import ProcessCorporate from "@/sections/ProcessCorporate";
@@ -12,7 +11,6 @@ import TaxBusiness from "@/sections/TaxBusiness";
 import Team from "@/sections/Team";
 import Welcome from "@/sections/Welcome";
 import WhyChooseUs from "@/sections/WhyChooseUs";
-import Works from "@/sections/Works";
 
 export default function Page() {
   return (
@@ -25,8 +23,6 @@ export default function Page() {
       <Team />
       <ProcessElearning />
       <ProcessCorporate />
-      {/* <HowItWork1 /> */}
-      {/* <Works /> */}
       <Pricing />
       <Service />
       <Contact />
